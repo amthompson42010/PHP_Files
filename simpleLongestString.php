5e15ec0b86e18a17101c8ce19b1d0af4b70b60ad
Alexander Mark Thompson <amthompson42010@gmail.com>
2016-01-24 10:15:37 -0600

    Update simpleLongestString.php
    Made the file more readable.

--- simpleLongestString.php
+++ simpleLongestString.php
@@ -1,7 +1,54 @@
 <?php
-string1 = "Nonstereotypic reversibly recriminator unjaded lerne mmerung uxoricidal unkidnapped boreas diplopod absorbed untemporary gareth alcoholysis jurisprudence unprocreated muse cleanness supercapital wizardry superette sybarite avestic tobe certificating. Scarify ephesian woolworth centuplicate dacha intermarried uncongenial harrietta quarriable muscat workingman shahaptian nonabjuratory calmly hag subobsolete lutheranism homologated superterrestrial incommunicably hertz graecising cloud misclassify glycogenase. Devitalization tweeddale clubhouses subreport grandview dean undergraduate preimitated merrimarauders i''d meshugana cyril changeability hopeh repenned namur prefabricate gorgerin precarnival bazaar dessau sightscreen prestudious frigidarium manta. Prandially noninsistence buchu slummier incliner malate ochering benedictionale horny whig quadruply tetrastich revetment carrefour lividly longueur realising craps postdoctoral unsportsmanly nonkinetic zamboanga phyllocladium unsolicitous agon. Methacetin conventual lord wipo tony arvol peridermic waspiness incloser invitational squinny semiautomatically desiderating antistatic huntsville meltingly unasked sealant dogmatist tapeless cestuses superduplication winsomeness weightlifting disciplinability. Kansa reluctivity cipherer essentializing gruemberger outbending nicholas onomasiology unvicious erysipelatous azerbaijanian inspectable ahungered highlight anachronic formal orem cyanotic unspectacular honkie unsmacked pneumatophore awnless batteler sofaing. Accutron gen defoe midst iceboating unsuppositive semicultured bemeaning conciliated conversation cyanohydrin farinaceous balaton leat cocktail utilitarianism vast dermatozoon uncontested pseudoangelical semipostal jot vendmiaire acknowledgment ironer. Mlg culturist unrationalised skua marivaux clianthus limnology hydroxyzine redeveloper attachï¿¥ï¾½ bend bandanaed bloody oversteadiness panchromatism unedifying geometrically nonthoracic lsd disputatious unnoteworthy intermix belike cavalerius quill. Zero barat preconstituent draying bentleyville groundedness aeacus tony aeolis mugwump existentialist conservatory unramped indiction lentamente oran bequeather subaggregation wavy crocked insertable checkmating undercutting calvina nonrated. Dichromat vilifying cartilage stettinius marshier sixteen fusula causatively petiolate clorinda scriptorium apostleship cotswolds unweeded clownishly doggedly symphyseal emplacing psephologist mccrae hellkite entertainingness jura unevadable premillennialist. Knotting femininity undeluding caird ethnos eurytus dumper vandalise dissipater philby overcontrite sauropod diorthosis contredanse backwardness scaring nibelung resourceful unacademical spokeshave meung pointlessly fornicatrices liturgic subhealth. Frousier doleritic fistulate authoritativeness mordacity sceptre statuary disingenuous preestimate flyweight martyrologist agrarianly teliospore benzaldehyde papalizing chilipepper chondrosarcomatous remembrance overskeptticism dayan sei solmisation corinthians joyful chastising.";
+  string1 = "Nonstereotypic reversibly recriminator unjaded 
+            lerne mmerung uxoricidal unkidnapped boreas diplopod 
+            absorbed untemporary gareth alcoholysis jurisprudence 
+            unprocreated muse cleanness supercapital wizardry 
+            superette sybarite avestic tobe certificating. 
+            Scarify ephesian woolworth centuplicate dacha intermarried 
+            uncongenial harrietta quarriable muscat workingman 
+            shahaptian nonabjuratory calmly hag subobsolete lutheranism 
+            homologated superterrestrial incommunicably hertz graecising 
+            cloud misclassify glycogenase. Devitalization tweeddale 
+            clubhouses subreport grandview dean undergraduate preimitated 
+            merrimarauders i''d meshugana cyril changeability hopeh 
+            repenned namur prefabricate gorgerin precarnival bazaar dessau 
+            sightscreen prestudious frigidarium manta. Prandially 
+            noninsistence buchu slummier incliner malate ochering 
+            benedictionale horny whig quadruply tetrastich revetment 
+            carrefour lividly longueur realising craps postdoctoral 
+            unsportsmanly nonkinetic zamboanga phyllocladium unsolicitous 
+            agon. Methacetin conventual lord wipo tony arvol peridermic 
+            waspiness incloser invitational squinny semiautomatically 
+            desiderating antistatic huntsville meltingly unasked sealant 
+            dogmatist tapeless cestuses superduplication winsomeness 
+            weightlifting disciplinability. Kansa reluctivity cipherer 
+            essentializing gruemberger outbending nicholas onomasiology 
+            unvicious erysipelatous azerbaijanian inspectable ahungered 
+            highlight anachronic formal orem cyanotic unspectacular honkie 
+            unsmacked pneumatophore awnless batteler sofaing. Accutron gen 
+            defoe midst iceboating unsuppositive semicultured bemeaning 
+            conciliated conversation cyanohydrin farinaceous balaton leat 
+            cocktail utilitarianism vast dermatozoon uncontested pseudoangelical 
+            semipostal jot vendmiaire acknowledgment ironer. Mlg culturist 
+            unrationalised skua marivaux clianthus limnology hydroxyzine redeveloper 
+            attachï¿¥ï¾½ bend bandanaed bloody oversteadiness panchromatism unedifying 
+            geometrically nonthoracic lsd disputatious unnoteworthy intermix belike 
+            cavalerius quill. Zero barat preconstituent draying bentleyville 
+            groundedness aeacus tony aeolis mugwump existentialist conservatory 
+            unramped indiction lentamente oran bequeather subaggregation wavy crocked 
+            insertable checkmating undercutting calvina nonrated. Dichromat vilifying 
+            cartilage stettinius marshier sixteen fusula causatively petiolate 
+            clorinda scriptorium apostleship cotswolds unweeded clownishly doggedly 
+            symphyseal emplacing psephologist mccrae hellkite entertainingness jura 
+            unevadable premillennialist. Knotting femininity undeluding caird ethnos 
+            eurytus dumper vandalise dissipater philby overcontrite sauropod diorthosis 
+            contredanse backwardness scaring nibelung resourceful unacademical 
+            spokeshave meung pointlessly fornicatrices liturgic subhealth. Frousier 
+            doleritic fistulate authoritativeness mordacity sceptre statuary 
+            disingenuous preestimate flyweight martyrologist agrarianly teliospore 
+            benzaldehyde papalizing chilipepper chondrosarcomatous remembrance 
+            overskeptticism dayan sei solmisation corinthians joyful chastising.";
   $words = explode(" ", $string1);
   $lengths = array_map('strlen', $words);
   echo "The longest string is " . max($lengths);
-  
 ?>
